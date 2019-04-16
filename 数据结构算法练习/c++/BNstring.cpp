648b3d0111a4bc7c97a5f98715a3dc41048d8232
zmubai <634825162@qq.com>
2019-04-16 22:08:49 +0800

    14最长公共前缀，第0个元素作为基准遍历字符，内循环1-末尾元素取元素对比，并判断是否越界，如果越界返回结果。或在外循环结束后返回结果

--- 数据结构算法练习/c++/BNstring.cpp
+++ 数据结构算法练习/c++/BNstring.cpp
@@ -111,3 +111,37 @@ bool isValid(string s) {
     return stt.empty();
 }
 
+/*
+ 编写一个函数来查找字符串数组中的最长公共前缀。
+ 
+ 如果不存在公共前缀，返回空字符串 ""。
+ 
+ 示例 1:
+ 
+ 输入: ["flower","flow","flight"]
+ 输出: "fl"
+ 示例 2:
+ 
+ 输入: ["dog","racecar","car"]
+ 输出: ""
+ 解释: 输入不存在公共前缀。
+ 说明:
+ 
+ 所有输入只包含小写字母 a-z 。
+ */
+///有使用字典树的解法
+string longestCommonPrefix(vector<string>& strs) {
+    if(strs.size() == 0) return "";
+    string x ="";
+    string fir = strs[0];
+    for (int i = 0; i < fir.length(); i ++) {
+        char dst = fir[i];
+        for (int j = 1; j < strs.size(); j ++) {
+            string sec = strs[j];
+            if(sec.length() - 1 < i || sec[i] != dst) return x;
+        }
+        ///添加1个dst到x末尾，append的用法。
+        x.append(1,dst);
+    }
+    return x;
+}
